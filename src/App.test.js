95d5e6f9962528b7990af250022a6e8c09cd2b5c
sirleneduarte <sirlene89@gmail.com>
2021-11-02 18:19:33 -0300

    Add Header component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const header = screen.getByTestId("header");
+  expect(header).toBeDefined();
 });
---
+++ src/components/header/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        
+        <div data-testid="header"><h1>Header</h1></div>
+    )
+}
+
+export default Header;
